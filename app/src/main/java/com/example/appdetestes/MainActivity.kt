dc02c21662a6fe4ea2de9b83d4829296bff631cb
Derlys <derlysr6@gmail.com>
2021-04-07 06:37:25 -0300

    adicionado funcionalidad para mostrar ImageView e não mais TextView, adicionado as images do Dice

--- app/src/main/java/com/example/appdetestes/MainActivity.kt
+++ app/src/main/java/com/example/appdetestes/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.appdetestes
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,9 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button1)
+        val imageOfDice: ImageView = findViewById(R.id.imageView2)
         rollButton.setOnClickListener {
             val dice = DiceRoller()
-            dice.roller().toString().also { textviewcentral.text = it }
+            when (dice.roller().toInt()){
+                1 -> imageOfDice.setImageResource(R.drawable.dice_1)
+                2 -> imageOfDice.setImageResource(R.drawable.dice_2)
+                3 -> imageOfDice.setImageResource(R.drawable.dice_3)
+                4 -> imageOfDice.setImageResource(R.drawable.dice_4)
+                5 -> imageOfDice.setImageResource(R.drawable.dice_5)
+                6 -> imageOfDice.setImageResource(R.drawable.dice_6)
+            }
+            //dice.roller().toString().also { textviewcentral.text = it }
         }
     }
 }
